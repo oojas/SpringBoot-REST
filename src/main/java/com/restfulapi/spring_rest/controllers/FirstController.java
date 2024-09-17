457a631d74339d3404866c6efb61564eba48f4e9
oojas <oojasgupta@gmail.com>
2024-09-17 20:20:10 +0530

    Added rest controllers

--- src/main/java/com/restfulapi/spring_rest/controllers/FirstController.java
+++ src/main/java/com/restfulapi/spring_rest/controllers/FirstController.java
@@ -1,10 +1,7 @@
 package com.restfulapi.spring_rest.controllers;
 
 import com.restfulapi.spring_rest.Modals.Order;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FirstController {
@@ -21,5 +18,17 @@ public class FirstController {
     @PostMapping(value = "/order-details")
     public String orderDetails(@RequestBody Order order){
         return order.toString();
+    }
+    @GetMapping(value = "/sayHello/{user-name}")
+    public String pathHello(@PathVariable("user-name") String name){
+        return "HIIIIIII "+name;
+
+    }
+    @GetMapping(value = "/action")
+    public String requestParam(
+            @RequestParam("user-name") String firstName,
+            @RequestParam("user-state") String state
+    ){
+        return "The username is "+firstName+" and the user lives in "+state;
     }
 }
